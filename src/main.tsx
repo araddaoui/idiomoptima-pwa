21e8cb5c485fb8ef42f3adba3cab01562ac85ef3
araddaoui <araddaoui@gmail.com>
2026-05-08 21:39:00 -0600

    Footer links now point to /terms.html and /privacy.html

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App.tsx';
-import Terms from './pages/Terms';
-import Privacy from './pages/Privacy';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/terms" element={<Terms />} />
-        <Route path="/privacy" element={<Privacy />} />
-      </Routes>
-    </BrowserRouter>
-  </StrictMode>
+    <App />
+  </StrictMode>,
 );
